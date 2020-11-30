a022304cba6d03362d01f00f927872480b1879aa
sspecht <985375+sspecht@users.noreply.github.com>
2020-11-30 14:40:30 +0100

    docs: created OFFIS develop branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,9 @@ module.exports = {
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
-  rules: {},
+  rules: {
+    'linebreak-style': 0,
+  },
   parserOptions: {
     parser: 'babel-eslint',
   },
