506fc6d603c384634a85c6c2aa04709a0c8c2c12
Niklas Labitzke <niklas.labitzke@uni-oldenburg.de>
2021-01-20 12:56:51 +0100

    created vaahessen algorith

--- src/store/modules/algorithm/algorithms/index.js
+++ src/store/modules/algorithm/algorithms/index.js
@@ -2,10 +2,12 @@ import cityblockApproveNeutralReject from './cityblock/approve-neutral-reject';
 import cityblockApprovePartlyReject from './cityblock/approve-partly-reject';
 import cityblockApprovePartlyLittleReject from './cityblock/approve-partly-little-reject';
 import hybrid from './hybrid/hybrid';
+import hybridVaahessen from './hybrid/hybrid-vaahessen';
 
 export default {
   'cityblock/approve-neutral-reject': cityblockApproveNeutralReject,
   'cityblock/approve-partly-reject': cityblockApprovePartlyReject,
   'cityblock/approve-partly-little-reject': cityblockApprovePartlyLittleReject,
   hybrid,
+  hybridVaahessen,
 };
